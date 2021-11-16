3259e9c9d000ee46f5ccca478d29755c0d0ce3d5
Paul Leydier <paul.leydier@compass-ft.com>
2021-11-17 00:18:41 +0100

    test: md5

---
+++ hashing/md5/md5_test.go
@@ -0,0 +1,24 @@
+package md5
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestMd5(t *testing.T) {
+	testCases := []struct {
+		in       string
+		expected string
+	}{
+		//{"", "d41d8cd98f00b204e9800998ecf8427e"},
+		{"The quick brown fox jumps over the lazy dog", "9e107d9d372bb6826bd81d3542a419d6"},
+		{"The quick brown fox jumps over the lazy dog.", "e4d909c290d0fb1ca068ffaddf22cbd0"},
+	}
+	for _, tc := range testCases {
+		res := Md5([]byte(tc.in))
+		result := hex.EncodeToString(res[:])
+		if result != tc.expected {
+			t.Fatalf("Md5(%s) = %s, expected %s", tc.in, result, tc.expected)
+		}
+	}
+}
